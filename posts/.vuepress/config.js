abfe49ec5969cb1fd3ef2d1b50bcbb3016f43a05
Rizki Ardian <radeite8@gmail.com>
2020-01-03 11:24:21 +0700

    Add google site verification

--- posts/.vuepress/config.js
+++ posts/.vuepress/config.js
@@ -8,6 +8,7 @@ module.exports = {
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }],
     ['meta', { name: 'keywords', content: 'unrizki, developer' }],
     ['meta', { name: 'author', content: 'unrizki' }],
+    ['meta', { name: 'google-site-verification', content: 'C6PUpooLizJmY5GMc5givDPgKhx9wQo_RBfWuC0PFtc' }]
   ],
   evergreen: true
 }
