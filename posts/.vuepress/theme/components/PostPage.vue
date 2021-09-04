43e874160a225b5506a09c78e6549084342cf6a7
Rizki Ardian <radeite8@gmail.com>
2021-09-04 11:44:18 +0700

    Update Post: remove Loader

--- posts/.vuepress/theme/components/PostPage.vue
+++ posts/.vuepress/theme/components/PostPage.vue
@@ -1,19 +1,10 @@
 <template>
   <div class="wrapper">
-    <section v-if="loading">
-      <loader />
-    </section>
-    <article
-      class="head"
-      :class="{ 'shown': !loading }"
-    >
+    <article class="head">
       <h2>{{ $page.title }}</h2>
       <b>{{ formatDate($page.frontmatter.date || $page.lastUpdated) }}</b>
     </article>
-    <article
-      class="content"
-      :class="{ 'shown': !loading }"
-    >
+    <article>
       <Content />
     </article>
   </div>
@@ -21,23 +12,8 @@
 
 <script>
 import { format, getYear } from 'date-fns'
-import Loader from './Loader'
 
 export default {
-  components: {
-    Loader
-  },
-
-  data: () => ({
-    loading: true
-  }),
-
-  mounted() {
-    setTimeout(() => {
-      this.loading = false
-    }, 1000)
-  },
-
   methods: {
     formatDate: date => format(
       new Date(date),
@@ -54,14 +30,6 @@ export default {
 
 .head {
   margin-bottom: 1.5em;
-  opacity: 0;
-  transform: scale(1.05) translateY(-0.5em);
-  transition: 0.5s cubic-bezier(0, 0, 0, 1.2);
-
-  &.shown {
-    opacity: 1;
-    transform: scale(1) translateY(0);
-  }
 
   h2 {
     margin-top: 0;
@@ -76,17 +44,6 @@ export default {
       font-weight: 700;
       margin-right: 0.5em;
     }
-  }
-}
-
-.content {
-  opacity: 0;
-  transform: translateY(-1em);
-  transition: 0.5s cubic-bezier(0, 0, 0, 1.2);
-
-  &.shown {
-    opacity: 1;
-    transform: translateY(0);
   }
 }
 </style>
