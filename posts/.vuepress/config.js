87b5ec6bd5f682b960ed55146e004637481476e9
Rizki Ardian <radeite8@gmail.com>
2022-04-11 00:52:20 +0700

    Fix google font load

--- posts/.vuepress/config.js
+++ posts/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
     ['meta', { name: 'author', content: 'unrizki' }],
     ['meta', { name: 'google-site-verification', content: 'C6PUpooLizJmY5GMc5givDPgKhx9wQo_RBfWuC0PFtc' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
-    ['link', { rel: 'stylesheet', href: 'https://fonts.gstatic.com', crossorigin: true }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true }],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap' }],
     ['link', { rel: 'stylesheet', href: 'https://api.fontshare.com/css?f[]=satoshi@400,700&display=swap' }],
     ['link', { rel: 'shortcut icon', href: '/favicon.ico' }]
