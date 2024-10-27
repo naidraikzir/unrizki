50e6c2702cf81942b43b01edf413b8caeed35c6e
Rizki Ardian <radeite8@gmail.com>
2024-10-27 11:08:29 +0700

    Move static meta and fonts to html template

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,23 +12,6 @@
 	let { children }: Props = $props();
 </script>
 
-<svelte:head>
-	<meta name="keywords" content="unrizki, developer" />
-	<meta name="author" content="unrizki" />
-	<meta name="google-site-verification" content="C6PUpooLizJmY5GMc5givDPgKhx9wQo_RBfWuC0PFtc" />
-
-	<link
-		href="https://api.fontshare.com/v2/css?f[]=cabinet-grotesk@700&display=swap"
-		rel="stylesheet"
-	/>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Crimson+Pro:ital,wght@0,300;1,300&display=swap"
-		rel="stylesheet"
-	/>
-</svelte:head>
-
 <div class="wrapper">
 	<Header />
 	<div class="content">
